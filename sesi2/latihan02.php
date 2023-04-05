839478336486ef61f7d2222b802524a644994475
Suputrajaya <127906364+Suputrajaya@users.noreply.github.com>
2023-04-05 08:04:44 +0800

    Add files via upload

---
+++ sesi2/latihan02.php
@@ -0,0 +1,11 @@
+<?php
+    $angka1 = 5;
+    $angka2 = 9;
+
+    if($angka1 == $angka2){
+        //bagian true
+        echo "nilai variable angka1 sama dengan angka2";;
+    }else{
+        //bagian false
+        echo "nilai variable angka1 tidak sama dengan variable angka2";
+    }
