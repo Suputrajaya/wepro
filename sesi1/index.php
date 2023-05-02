56deaa52fdd327faf8abb489cbaea6e4d8ac3e60
Suputrajaya <127906364+Suputrajaya@users.noreply.github.com>
2023-05-02 22:35:01 +0800

    Add files via upload

---
+++ sesi1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Sesi 1</title>
+</head>
+<body>
+    <h3>Latihan Web Programing</h3>
+
+    <script>
+        document.write("Latihan Web Programing - dg JavaScript");
+    </script>
+
+    <br>
+
+    <?php
+    echo "Latihan Web Programing - dg PHP";
+    ?>
+</body>
+</html>
